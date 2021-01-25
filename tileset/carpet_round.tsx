f03abcb2da650529690f6c9716f7e6547f7e75a3
wasreligionslehrersomachen <77389934+wasreligionslehrersomachen@users.noreply.github.com>
2021-01-25 22:02:07 +0100

    Create carpet_round.tsx

---
+++ tileset/carpet_round.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="carpet_round" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <editorsettings>
+  <export target="../" format=""/>
+ </editorsettings>
+ <image source="carpet_round.png" width="96" height="96"/>
+</tileset>
